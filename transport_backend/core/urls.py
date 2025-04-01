e1cac914e25862675dede3bdc9a2bef78606729a
Eden Iyanda <edeniyanda@gmail.com>
2025-04-01 13:08:36 +0100

    Implement RESTful API endpoints for City, BusPark, Route, and Booking with viewsets and router configuration

--- transport_backend/core/urls.py
+++ transport_backend/core/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
 
+router = DefaultRouter()
+router.register('cities', CityViewSet)
+router.register('parks', BusParkViewSet)
+router.register('routes', RouteViewSet)
+router.register('bookings', BookingViewSet)
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include(router.urls)),
 ]
--- transport_backend/core/views.py
+++ transport_backend/core/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import viewsets, permissions
+from .models import City, BusPark, Route, IndirectRoute, Booking
+from .serializers import *
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the core index.")
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class BusParkViewSet(viewsets.ModelViewSet):
+    queryset = BusPark.objects.all()
+    serializer_class = BusParkSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user, status='confirmed')
