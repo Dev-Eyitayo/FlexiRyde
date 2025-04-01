b334025f994ea52c958e7b350e645e060ba3c397
Eden Iyanda <edeniyanda@gmail.com>
2025-04-01 13:00:52 +0100

    Add core models and serializers for City, BusPark, Route, IndirectRoute, and Booking

--- transport_backend/core/admin.py
+++ transport_backend/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import City, BusPark, Route, IndirectRoute, Booking
 
 # Register your models here.
+admin.site.register(City)
+admin.site.register(BusPark)    
+admin.site.register(Route)
+admin.site.register(IndirectRoute)
+admin.site.register(Booking)
