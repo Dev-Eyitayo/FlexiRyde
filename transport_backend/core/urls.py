d7b96d3c68a4e69fa036f1b2670b0b04bd2526e9
Eden Iyanda <edeniyanda@gmail.com>
2025-04-08 21:27:35 +0100

    feat: Add park-related API endpoints for buses, routes, and trips in urls.py

--- transport_backend/core/urls.py
+++ transport_backend/core/urls.py
@@ -1,8 +1,10 @@
+# core/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     CityViewSet, BusParkViewSet, RouteViewSet, BookingViewSet,
-    TripSearchAPIView, TripViewSet, BookingCreateAPIView, BusViewSet
+    TripSearchAPIView, TripViewSet, BookingCreateAPIView, BusViewSet,
+    ParkBusesView, ParkRoutesView, ParkTripsView, TripCreateView
 )
 
 router = DefaultRouter()
@@ -16,6 +18,9 @@ router.register('buses', BusViewSet)
 urlpatterns = [
     path('trips/search/', TripSearchAPIView.as_view(), name='trip-search'),
     path("bookings/create/", BookingCreateAPIView.as_view(), name="booking-create"),
+    path('parks/<int:park_id>/buses/', ParkBusesView.as_view(), name='park_buses'),
+    path('parks/<int:park_id>/routes/', ParkRoutesView.as_view(), name='park_routes'),
+    path('parks/<int:park_id>/trips/create/', TripCreateView.as_view(), name='trip_create'),
+    path('parks/<int:park_id>/trips/', ParkTripsView.as_view(), name='park_trips'),
     path('', include(router.urls)),
-    
 ]
