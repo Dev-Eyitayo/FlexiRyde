047f3439fcec2f39d3aeb246c65486dae7e03fae
Eden Iyanda <edeniyanda@gmail.com>
2025-04-10 14:05:17 +0100

    fix: Simplify TripListSerializer methods and clean up Meta class definition

--- transport_backend/core/serializers.py
+++ transport_backend/core/serializers.py
@@ -69,40 +69,18 @@ class TripListSerializer(serializers.ModelSerializer):
     route = serializers.SerializerMethodField()
     departure_time = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Trip
-        fields = [
-            'id',
-            'travel_date',
-            'departure_time',
-            'seat_price',
-            'bus',
-            'route',
-        ]
-
     def get_bus(self, obj):
-        return {
-            "number_plate": obj.bus.number_plate,
-            "total_seats": obj.bus.total_seats,
-        }
+        return {"number_plate": obj.bus.number_plate, "total_seats": obj.bus.total_seats}
 
     def get_route(self, obj):
         return {
-            "origin_park": {
-                "id": obj.route.origin_park.id,
-                "name": obj.route.origin_park.name,
-            },
-            "destination_park": {
-                "id": obj.route.destination_park.id,
-                "name": obj.route.destination_park.name,
-            },
+            "origin_park": {"id": obj.route.origin_park.id, "name": obj.route.origin_park.name},
+            "destination_park": {"id": obj.route.destination_park.id, "name": obj.route.destination_park.name},
             "distance_km": obj.route.distance_km,
         }
-    def get_departure_time(self, obj):
-        if not obj.departure_time:
-            return None
-        return obj.departure_time.strftime('%I:%M %p')
 
+    def get_departure_time(self, obj):
+        return obj.departure_time.strftime('%I:%M %p') if obj.departure_time else None
 
 class BookingCreateSerializer(serializers.ModelSerializer):
     class Meta:
