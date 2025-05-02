5c2d73099e126a402bb4f7038b1aaa041f7869b1
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-05-02 06:50:58 +0100

    Update

--- transport_backend/transport_backend/settings.py
+++ transport_backend/transport_backend/settings.py
@@ -141,17 +141,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'transport_backend.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
