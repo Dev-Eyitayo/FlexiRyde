acfb6b0c277b2bd9850808365f6017737070faa3
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-07-07 11:45:56 +0100

    Changed db to postgres

--- transport_backend/transport_backend/settings.py
+++ transport_backend/transport_backend/settings.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, '.env'))
@@ -137,11 +138,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'transport_backend.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,  # Optional: persistent connections
+        ssl_require=True   # Optional: require SSL if your DB needs it
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
