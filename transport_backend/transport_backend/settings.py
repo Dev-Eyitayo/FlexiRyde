147c7312e660ec8145da08de8d3b98905484fd20
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-07-08 13:06:07 +0100

    Documented main with swagger

--- transport_backend/transport_backend/settings.py
+++ transport_backend/transport_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'social_django',
     'core',
     'accounts',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -138,21 +139,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'transport_backend.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {
-    "default": dj_database_url.parse(
-        DATABASE_URL,
-        conn_max_age=600,  # Optional: persistent connections
-        ssl_require=True   # Optional: require SSL if your DB needs it
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASES = {
+#     "default": dj_database_url.parse(
+#         DATABASE_URL,
+#         conn_max_age=600,  # Optional: persistent connections
+#         ssl_require=True   # Optional: require SSL if your DB needs it
+#     )
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
