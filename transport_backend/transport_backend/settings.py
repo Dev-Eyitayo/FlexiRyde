4f78d2e877b8c0303ee36762d186a11f10a9d6cc
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-07-09 12:55:38 +0100

    Added another origin

--- transport_backend/transport_backend/settings.py
+++ transport_backend/transport_backend/settings.py
@@ -58,7 +58,8 @@ ROOT_URLCONF = 'transport_backend.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://flexiryde.vercel.app",
-    "http://127.0.1.5500"
+    "http://127.0.1.5500/",
+    "http://127.0.1.5500",
 ]
 
 
